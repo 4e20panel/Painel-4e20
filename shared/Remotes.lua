d5d800ca6c6be63bff531ea0ffe6cbc328e0b716
4e20panel <felipenatal021@gmail.com>
2026-04-19 12:01:45 -0300

    Update Remotes.lua

--- shared/Remotes.lua
+++ shared/Remotes.lua
@@ -1,13 +1,1 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local folder = Instance.new("Folder")
-folder.Name = "Remotes"
-folder.Parent = ReplicatedStorage
-
-local setTag = Instance.new("RemoteEvent")
-setTag.Name = "SetTag"
-setTag.Parent = folder
-
-local sync = Instance.new("RemoteEvent")
-sync.Name = "SyncPanel"
-sync.Parent = folder
